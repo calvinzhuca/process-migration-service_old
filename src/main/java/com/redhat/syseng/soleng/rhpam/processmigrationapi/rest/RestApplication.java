60a0d9369c3496eab5ad8cf3be728bfa3ee5c844
calvin zhu <calvinzhu@hotmail.com>
2018-07-24 16:13:32 -0400

    update

--- src/main/java/com/redhat/syseng/soleng/rhpam/processmigrationapi/rest/RestApplication.java
+++ src/main/java/com/redhat/syseng/soleng/rhpam/processmigrationapi/rest/RestApplication.java
@@ -1,16 +1,9 @@
 package com.redhat.syseng.soleng.rhpam.processmigrationapi.rest;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Response;
 
 @ApplicationPath("/")
 public class RestApplication extends Application {
-	@GET
-	@Produces("text/plain")
-	public Response doGet() {
-		return Response.ok("Hello from RestApplication!").build();
-	}
+
 }
